0450f94763a042bd9b0dd3b683628c1ad1fbea71
NoNelso <jasonr4nd@gmail.com>
2019-01-10 17:09:03 -0500

    planned topdown functions to be filled in with classes later.

--- sketch.js
+++ sketch.js
@@ -1,7 +1,15 @@
 function setup() {
   createCanvas(600, 500);
+  //create circle with
+  //radius 45, dist from center 0, rotationspeed 0, angle random val < 2 PI
+  sun = new Planet(45, 0, 0, random(TWO_PI));
+  //create 5 rotating bodies about sun with depth of spawn 1
+  sun.spawnSpiners(5, 1);
 }
 
 function draw() {
-
+  background(21); //black space
+  translate(width / 2, height / 2); //sun in center
+  sun.show(); //show solar system
+  sun.spin(); //itterate solar system
 }
