a0cc9217b1a569ff125e59c0caa9bc31935830b8
NoNelso <jasonr4nd@gmail.com>
2019-02-18 16:03:42 -0500

    'this.', for loop overflow, spin/orbit mismatch, scaling, & array call #

--- planet.js
+++ planet.js
@@ -11,26 +11,26 @@ function Planet(radius, distance, speed, angle) {
     rotate(this.a);
     translate(this.d, 0);
     ellipse(0, 0, this.r * 2);
-    for (i = planets.length; i > 0; --i) this.planets[i].show();
+    for (i = this.planets.length - 1; i >= 0; --i) this.planets[i].show();
     pop();
   }
 
   this.spin = function() {
-    this.a += this.s; // spin based on speed
-    for (i = planets.length; i > 0; --i)
-      this.planets[i].orbit(); // recursively call through dependents
+    this.a += this.s; // base spin rate on speed
+    for (i = this.planets.length - 1; i >= 0; --i) //recursive check dependants
+      this.planets[i].spin(); //re-call function
   }
 
   this.spawnSpiners = function(childNum, level) {
     for (i = 0; i < childNum; ++i) { //create planets = to children number
-      var rad = this.r / (level * 2);
-      var dis = floor(random(50, 150));
-      var spd = random(-1, 1);
-      let ang = random(TWO_PI);
+      var rad = this.r / (level + 1);
+      var dis = floor(random(100, 150) / level);
+      var spd = random(-.1, .1);
+      var ang = random(TWO_PI);
       this.planets.push(new Planet(rad, dis, spd, ang));
       if (level < 3) { //if level not at cap spawn again
-        var num = floor(random(0, 4));
-        this.planets.spawnSpiners(num, level + 1);
+        //var num = floor(random(0, 4))
+        this.planets[i].spawnSpiners(5 - level, level + 1);
       }
     }
   }
