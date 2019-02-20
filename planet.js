0d1196bec19fc2acf455676b8a24276566478ef7
NoNelso <jasonr4nd@gmail.com>
2019-02-20 00:53:43 -0500

    added qualifier to spawner to not overspawn planets.

--- planet.js
+++ planet.js
@@ -8,7 +8,7 @@ function Planet(radius, distance, speed, angle, level) {
   this.planets = [];
 
   this.show = function() { //save state, move center, show, and reload state
-    //console.log(sun.planets);
+    console.log(sun.planets);
     push();
     rotate(this.a);
     translate(this.d, 0);
@@ -29,15 +29,16 @@ function Planet(radius, distance, speed, angle, level) {
   }
 
   this.spawnSpiners = function(childNum, lv) {
-    //console.log("C#", childNum, "LV", lv);
-    for (i = 0; i < childNum; ++i) { //create planets by children number per lv
-      var rad = ceil(this.r / (lv + .3)); //each level is slightly smaler than parent
-      var dis = floor(random(100, 150) / lv); //each planet rand dist from parent
-      var spd = random(-.2, .2); //each planet at rand orbit speed
-      var ang = random(TWO_PI); //no planet should spawn right ontop of brother
-      var lev = lv;
-      this.planets.push(new Planet(rad, dis, spd, ang, lev));
-    }
+    console.log("C#", childNum, "LV", lv);
+    if (childNum - this.planets.length > 0)
+      for (i = 0; i < childNum; ++i) { //create planets by children number per lv
+        var rad = ceil(this.r / (lv + .3)); //each level is slightly smaler than parent
+        var dis = floor(random(100, 150) / lv); //each planet rand dist from parent
+        var spd = random(-.2, .2); //each planet at rand orbit speed
+        var ang = random(TWO_PI); //no planet should spawn right ontop of brother
+        var lev = lv;
+        this.planets.push(new Planet(rad, dis, spd, ang, lev));
+      }
     if (lv < 3) { //if level not at cap spawn again
       for (j = 0; j < childNum; ++j) {
         this.planets[j].spawnSpiners(5 - lv, 1 + lv);
